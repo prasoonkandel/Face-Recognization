3117ad4ba92ad3338adf6bad41b763886c036a8d
Prasoon202 <prasoonkandel1029@gmail.com>
2025-06-22 18:19:29 +0545

    done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-img_name = "group.png"
-img_path = f"./{img_name}"
+img_name = "man.png"
+img_path = f"./Images/{img_name}"
 img = cv2.imread(img_path)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
